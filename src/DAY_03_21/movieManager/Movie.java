a18c88b90bc010efc9e5836d31edc4e198fb6d59
Hany-Kim <hanykim1210@gmail.com>
2024-03-21 18:00:37 +0900

    과제 class 생성

--- src/DAY_03_21/movieManager/Movie.java
+++ src/DAY_03_21/movieManager/Movie.java
@@ -1,4 +1,14 @@
 package DAY_03_21.movieManager;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Movie {
+    private String title;
+    private String genre;
+    private String director;
+    private String mainActor;
+    private String releaseDate;
 }
