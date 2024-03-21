31d6b13cacd3776c61f762be8073b8d377f6da82
Hany-Kim <hanykim1210@gmail.com>
2024-03-21 09:48:36 +0900

    commit test

---
+++ src/DAY_03_21/StringExample.java
@@ -0,0 +1,30 @@
+package DAY_03_21;
+
+public class StringExample {
+    public static void main(String[] args) throws Exception {
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = new String("hello");
+        String s4 = new String("hello");
+        System.out.println(s1 == s2);
+        System.out.println(s2 == s3);
+        System.out.println(s3 == s4);
+        System.out.println(s1.toUpperCase());
+        s1 = s1.toLowerCase();
+        System.out.println(s1);
+        System.out.println(s3.concat("world"));
+        System.out.println(s3);
+        System.out.println();
+        StringBuilder str = new StringBuilder("hello");
+        str.append("world");
+        System.out.println(str);
+
+        byte[] buffer = new byte[100];
+        System.out.println("입력:");
+        int readCount = System.in.read(buffer); // main에 throws Exception 추가
+        System.out.println(buffer);
+        String input = new String(buffer, 0, readCount);
+        System.out.println(input);
+
+    }
+}
